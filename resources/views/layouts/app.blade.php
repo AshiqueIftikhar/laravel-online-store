47335ef528733ed00afad236e276cd374b750645
AshiqueIftikhar <ashique.iftikhar@gmail.com>
2024-05-16 13:24:13 +0600

    index and about view added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
       </div>
     </div>
   </nav>
-  <header class="bg-warning text-white text-center py-1">
+  <header class="bg-primary text-white text-center py-1">
     <div class="container d-flex align-items-center flex-column">
       <h2>@yield('subtitle', 'A Laravel Online Store')</h2>
     </div>
