5fc5ae56eec51efa943a9611815dba273601ec91
AshiqueIftikhar <ashique.iftikhar@gmail.com>
2024-05-16 20:33:04 +0600

    adding dummy data in ProductConctroller for products

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,5 @@ use App\Http\Controllers\HomeController;
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
 Route::get('/about', [HomeController::class, 'about'])->name('home.about');
+Route::get('/products', [ProductController::class, 'index'])->name('product.index');
+Route::get('/proucts/{id}', [ProductController::class, 'show'])->name('product.show');
