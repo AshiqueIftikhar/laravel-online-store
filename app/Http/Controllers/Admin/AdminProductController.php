9cf27231442f617f6b4f90f8fed643e13cdc5209
AshiqueIftikhar <ashique.iftikhar@gmail.com>
2024-05-17 20:29:22 +0600

    image field created and image stored in storage

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -32,6 +32,13 @@ class AdminProductController extends Controller
         $newProduct->setImage("game.png");
         $newProduct->save();
 
+        if($request->hasFile('image')){
+            $imageName = $newProduct->getId().".".$request->file('image')->extension();
+            Storage::disk('public')->put($imageName, file_get_contents($request->file('image')->getRealPath()));
+            $newProduct->setImage($imageName);
+            $newProduct->save();
+        }
+
         return back();
     }
 }
